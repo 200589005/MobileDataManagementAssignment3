048b2fb3b68504adbef117fc8cec3e004377c022
Mitul Patel <mitulyaar92@gmail.com>
2024-07-15 15:44:21 -0400

    initial commit

---
+++ MovieSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchApp
+//
+//  Created by Mitul Patel on 15/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
